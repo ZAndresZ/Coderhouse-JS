f9b24b92593e61f8335c7c41ba5878ed216229a2
ZAndresZ <48682612+ZAndresZ@users.noreply.github.com>
2021-08-30 13:26:46 -0300

    desafio1

---
+++ desafio1/js/extension.js
@@ -0,0 +1,30 @@
+let nombre;
+const centroFromacion = 'CoderHouse';
+let numero1,numero2;
+let resultado;
+let texto1, texto2;
+
+nombre = prompt("1 - Introduzca su nombre por favor")
+alert("Hola " + nombre + ", como estas? Bienvenido a " + centroFromacion)
+
+
+alert("2 - Por favor introduzca un numero para concatenarlo con '10'");
+numero1 = prompt ("Ingrese el numero:");
+numero2 = 10;
+resultado = numero1 + numero2;
+alert(numero1 + " + " + numero2 + " = " + resultado);
+
+alert("3 - Por favor introduzca un numero para sumarlo con 100");
+numero1 = parseFloat(prompt("Ingrese el numero:"));
+numero2 = 10;
+resultado = numero1 + numero2;
+alert(numero1 + " + " + numero2 + " = " + resultado);
+console.log(numero1 + " + " + numero2 + " = " + resultado);
+
+texto1 = prompt("4 - Introduzca el texto");
+texto2 = prompt("Introduzca otro texto");
+alert("El resultado de la concatenacion es: " + texto1 + " " + texto2);
+console.log("El resultado de la concatenacion es: " + texto1 + " " + texto2);
+
+console.log(typeof(texto1));
+console.log(typeof(texto2));
